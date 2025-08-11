36cca3808e921ce47566b9987134cedb9707f44e
SyA <herrysans@yahoo.com>
2025-08-11 07:35:08 +0700

    working : home page

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useAppearance } from '@/composables/useAppearance';
-import { ChevronDown, DoorOpen, Moon, Sun } from 'lucide-vue-next';
+import { Link } from '@inertiajs/vue3';
+import { ChevronDown, DoorOpen, Menu, Moon, Sun, X } from 'lucide-vue-next';
 
 const { updateAppearance, appearance } = useAppearance();
 
@@ -11,110 +12,191 @@ const toggleAppereance = () => {
 </script>
 
 <template>
-    <nav class="border-gray-200 bg-gray-50 py-2 dark:bg-gray-900">
+    <nav class="border-gray-200 bg-gradient-to-r from-primary-500 to-secondary-400 dark:border-gray-700 dark:bg-gray-800">
         <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-4 py-1">
             <div class="relative -top-8 z-50">
-                <a
+                <Link
                     href="https://flowbite.com"
-                    class="absolute top-0 flex h-32 w-32 items-center justify-center rounded-b-2xl bg-white shadow dark:bg-gray-900"
+                    class="absolute top-0 flex h-36 w-36 items-center justify-center rounded-b-2xl bg-background shadow dark:bg-background"
                 >
-                    <img src="/placeholder-assets/logo.png" class="h-16 w-16" alt="Flowbite Logo" />
-                </a>
+                    <img src="/placeholder-assets/logo.png" class="h-24 w-24" alt="Flowbite Logo" />
+                </Link>
             </div>
             <div class="flex items-center gap-3">
                 <div class="relative flex items-center justify-center">
                     <label class="inline-flex cursor-pointer items-center">
                         <input @change="toggleAppereance" type="checkbox" :checked="appearance === 'light' ? false : true" class="peer sr-only" />
                         <div
-                            class="peer relative h-6 w-11 rounded-full bg-gray-200 peer-checked:bg-blue-600 peer-focus:ring-4 peer-focus:ring-blue-300 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-blue-600 dark:peer-focus:ring-blue-800"
+                            class="peer relative h-6 w-11 rounded-full bg-base-50 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:bg-secondary-400 after:transition-all after:content-[''] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full"
                         >
                             <Moon
-                                fill="text-gray-900"
-                                class="absolute right-[3px] bottom-[4px] z-10 h-4 w-4 text-gray-900 opacity-0 dark:opacity-100"
+                                fill="currentColor"
+                                class="absolute right-[3px] bottom-[4px] z-10 h-4 w-4 text-base-50 opacity-0 dark:opacity-100"
                             />
-                            <Sun
-                                fill="text-gray-900"
-                                class="absolute bottom-[3.5px] left-[3.5px] z-10 h-4 w-4 text-gray-900 opacity-100 dark:opacity-0"
-                            />
+                            <Sun fill="currentColor" class="absolute bottom-[4px] left-[4px] z-10 h-4 w-4 text-base-50 opacity-100 dark:opacity-0" />
                         </div>
-                        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+                        <span class="ms-3 text-sm font-medium text-foreground"></span>
                     </label>
                 </div>
-                <a
+                <Link
                     href="#"
-                    class="inline-flex items-center rounded-lg border border-gray-200 bg-white px-5 py-2 text-center text-xs font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    class="inline-flex items-center rounded-lg border border-border bg-background px-5 py-2 text-center text-xs font-medium text-foreground hover:bg-muted focus:ring-4 focus:ring-ring/20 focus:outline-none"
                 >
-                    Connect <DoorOpen class="ms-2 h-4 w-4" />
-                </a>
+                    Sistem <DoorOpen class="ms-2 h-4 w-4" />
+                </Link>
             </div>
         </div>
     </nav>
-    <nav class="sticky top-0 z-20 mx-auto flex bg-white dark:bg-gray-800 shadow-md">
-        <div class="mx-auto px-4 py-4">
+    <nav class="sticky top-0 z-20 mx-auto hidden bg-background shadow-md lg:flex">
+        <div class="mx-auto px-4">
             <div class="flex items-center">
-                <ul class="mt-0 flex flex-row space-x-8 text-sm font-medium rtl:space-x-reverse">
-                    <li class="hover:opacity-70">
-                        <a href="#" class="text-gray-900 dark:text-white" aria-current="page">Home</a>
+                <ul class="mt-0 flex flex-row text-sm font-medium rtl:space-x-reverse">
+                    <li class="group/nav-link cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="text-foreground" aria-current="page">Beranda</Link>
                     </li>
 
-                    <li class="hover:opacity-70">
-                        <a href="#" class="text-gray-900 dark:text-white">Profil</a>
+                    <li class="group/nav-link cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="text-foreground">Profil</Link>
                     </li>
-                    <li class="hover:opacity-70">
-                        <a href="#" class="text-gray-900 dark:text-white">Cabor</a>
+                    <li class="group/nav-link cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="text-foreground">Cabor</Link>
                     </li>
-                    <li class="hover:opacity-70">
-                        <a href="#" class="text-gray-900 dark:text-white">Event</a>
+                    <li class="group/nav-link cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="text-foreground">Event</Link>
                     </li>
-                    <li class="hover:opacity-70">
-                        <a href="#" class="text-gray-900 dark:text-white">Kontak</a>
-                    </li>
-                    <li class="hover:opacity-70">
-                        <a
-                            href="#"
-                            data-popover-placement="bottom"
-                            data-popover-target="popover-nav-publication"
-                            class="flex gap-2 text-gray-900 dark:text-white"
+                    <li class="group/nav-link relative cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="flex gap-2 text-foreground group-hover/nav-link:opacity-80"
                             >Publikasi <ChevronDown class="h-5 w-5" />
-                        </a>
+                        </Link>
+                        <div
+                            id="dropdownHover"
+                            class="absolute left-1/2 z-10 mt-4.5 w-44 -translate-x-1/2 divide-y divide-border overflow-hidden rounded-b-lg bg-card opacity-0 shadow-sm group-hover/nav-link:opacity-100"
+                        >
+                            <ul class="py-2 text-sm text-card-foreground" aria-labelledby="dropdownHoverButton">
+                                <li>
+                                    <Link href="#" class="block px-4 py-2 hover:bg-foreground/10">Fotografi</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" class="block px-4 py-2 hover:bg-foreground/10">Berita</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="group/nav-link cursor-pointer border-b-2 border-transparent px-4 py-4 hover:border-foreground">
+                        <Link href="#" class="text-foreground">Kontak</Link>
                     </li>
                 </ul>
             </div>
-            <div class="popovers">
-                <div
-                    data-popover
-                    id="popover-nav-publication"
-                    role="tooltip"
-                    class="invisible absolute z-10 inline-block w-96 rounded-lg border border-gray-200 bg-white text-sm text-gray-500 opacity-0 shadow-xs transition-opacity duration-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400"
-                >
-                    <div
-                        class="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6 dark:border-gray-700 dark:bg-gray-800"
+        </div>
+    </nav>
+    <nav class="sticky top-0 z-20 mx-auto flex justify-end bg-background shadow-md lg:hidden">
+        <div class="me-8 rounded-lg py-4 text-center">
+            <Menu
+                data-drawer-target="drawer-navigation"
+                data-drawer-toggle="drawer-navigation"
+                data-drawer-placement="right"
+                aria-controls="drawer-navigation"
+                class="h-8 w-8 cursor-pointer text-muted-foreground hover:text-foreground"
+            />
+        </div>
+    </nav>
+    <!-- Navigation drawer components -->
+    <div
+        id="drawer-navigation"
+        class="fixed top-0 right-0 z-[60] h-screen w-80 translate-x-full overflow-y-auto bg-background p-4 transition-transform duration-500"
+        tabindex="-1"
+        aria-labelledby="drawer-navigation-label"
+    >
+        <button
+            type="button"
+            data-drawer-hide="drawer-navigation"
+            aria-controls="drawer-navigation"
+            class="absolute end-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-muted-foreground hover:bg-muted hover:text-foreground"
+        >
+            <X class="h-5 w-5 font-thin" />
+            <span class="ms-1 font-semibold">Tutup</span>
+        </button>
+        <div class="mt-4 overflow-y-auto py-4">
+            <ul class="space-y-2 font-medium">
+                <li>
+                    <Link href="#" class="group flex items-center rounded-lg p-2 text-foreground hover:bg-base-100">
+                        <span class="ms-3">Beranda</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#" class="group flex items-center rounded-lg p-2 text-foreground hover:bg-base-100">
+                        <span class="ms-3">Profil</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#" class="group flex items-center rounded-lg p-2 text-foreground hover:bg-base-100">
+                        <span class="ms-3">Cabor</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#" class="group flex items-center rounded-lg p-2 text-foreground hover:bg-base-100">
+                        <span class="ms-3">Event</span>
+                    </Link>
+                </li>
+                <li>
+                    <button
+                        type="button"
+                        class="group flex w-full items-center rounded-lg p-2 text-base text-foreground transition duration-75 hover:bg-base-100"
+                        aria-controls="dropdown-example"
+                        data-collapse-toggle="dropdown-example"
                     >
-                        <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-                            <li class="pb-3 sm:pb-4">
-                                <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                                    <div class="min-w-0 flex-1">
-                                        <p class="truncate text-sm font-medium text-gray-900 dark:text-white">Neil Sims</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                                    <div class="min-w-0 flex-1">
-                                        <p class="truncate text-sm font-medium text-gray-900 dark:text-white">Bonnie Green</p>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                        <span class="ms-3 flex-1 text-left whitespace-nowrap rtl:text-right">Publikasi</span>
+                        <ChevronDown class="h-5 w-5 flex-shrink-0 text-muted-foreground transition duration-75 group-hover:text-foreground" />
+                    </button>
+                    <ul id="dropdown-example" class="hidden space-y-2 py-2">
+                        <li>
+                            <Link
+                                href="#"
+                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-foreground transition duration-75 hover:bg-base-100"
+                                >Fotografi</Link
+                            >
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-foreground transition duration-75 hover:bg-base-100"
+                                >Berita</Link
+                            >
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <Link href="#" class="group flex items-center rounded-lg p-2 text-foreground hover:bg-base-100">
+                        <span class="ms-3">Kontak</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </div>
 
-                    <div data-popper-arrow></div>
+    <main class="app">
+        <div class="-mt-3 h-[70vh] max-h-[600px] w-full">
+            <div class="h-full w-full">
+                <div class="relative h-full w-full">
+                    <slot name="pageBackground" />
+                    <div class="relative mx-auto h-full w-full max-w-screen-xl px-4">
+                        <div class="absolute top-1/2 left-0 max-w-screen-xl -translate-y-1/2 px-4">
+                            <nav class="mb-2 flex" aria-label="Breadcrumb">
+                                <slot name="pageBreadcrumb" />
+                            </nav>
+                            <h1 class="mb-2 text-3xl font-extrabold text-base-50 md:text-5xl"><slot name="pageTitle" /></h1>
+                            <p class="mb-6 text-lg font-normal text-base-50">
+                                <slot name="pageDescription" />
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </nav>
-
-    <main class="app">
-        <slot />
+        <div class="relative -mt-20 rounded-t-4xl bg-background p-5">
+            <div class="mx-auto max-w-screen-xl min-h-80">
+                <slot name="content" />
+            </div>
+        </div>
     </main>
 </template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
-import { Head } from '@inertiajs/vue3';
-import { ArrowRight, ChevronLeftCircle, ChevronRightCircle } from 'lucide-vue-next';
-import Swiper from 'swiper/bundle';
+import { Head, Link } from '@inertiajs/vue3';
+import { Megaphone, SquareArrowOutUpRight } from 'lucide-vue-next';
 import { onMounted } from 'vue';
 
 defineOptions({
@@ -11,22 +10,7 @@ defineOptions({
     },
 });
 
-onMounted(() => {
-    // init Swiper:
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-});
+onMounted(() => {});
 </script>
 
 <template>
@@ -35,55 +19,101 @@ onMounted(() => {
             <title>Home</title>
             <meta name="description" content="Home page description" />
         </Head>
-        <div class="-mt-3 h-[60vh] max-h-[600px] w-full">
-            <div class="swiper h-full w-full">
-                <!-- Additional required wrapper -->
-                <div class="swiper-wrapper">
-                    <!-- Slides -->
-                    <div
-                        class="swiper-slide bg-black/50 bg-[url('/placeholder-assets/storage/jumbotron/bg-1.jpg')] bg-cover bg-no-repeat bg-blend-darken"
-                    >
-                        <div class="h-full w-full relative">
-                            <div class="absolute left-1/2 top-1/2 -translate-1/2  max-w-screen-xl px-4 text-center">
-                                <h1 class="mb-4 text-4xl leading-none font-extrabold tracking-tight text-white">
-                                    We invest in the world’s potential
-                                </h1>
-                                <p class="mb-8 text-lg font-normal text-gray-300">
-                                    Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and
-                                    drive economic growth.
-                                </p>
-                                <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                                    <a
-                                        href="#"
-                                        class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-                                    >
-                                        Get started
-                                        <ArrowRight class="ms-2 h-4 w-4" />
-                                    </a>
-                                    <a
-                                        href="#"
-                                        class="inline-flex items-center justify-center rounded-lg border border-white px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-400 sm:ms-4"
-                                    >
-                                        Learn more
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        class="swiper-slide bg-black/50 bg-[url('/placeholder-assets/storage/jumbotron/bg-2.jpg')] bg-cover bg-no-repeat bg-blend-darken"
-                    >
-                        Slide 2
-                    </div>
-                </div>
-                <!-- If we need pagination -->
-                <div class="swiper-pagination"></div>
 
-                <!-- If we need navigation buttons -->
-                <div class="swiper-button-prev"><ChevronLeftCircle class="w-8 h-8" /> </div>
-                <div class="swiper-button-next"><ChevronRightCircle class="w-8 h-8" /> </div>
+        <!-- Top Background -->
+        <template #pageBackground>
+            <div
+                class="absolute h-full w-full bg-black/50 bg-[url('/placeholder-assets/storage/jumbotron/bg3.jpg')] bg-cover bg-no-repeat bg-blend-darken"
+            ></div>
+        </template>
+        <!-- Breadcrumb -->
+        <template #pageBreadcrumb>
+            <!-- <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li class="inline-flex items-center">
+                    <a href="#" class="inline-flex items-center text-sm font-medium text-base-100 hover:text-primary">
+                        <svg class="me-2.5 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+                            />
+                        </svg>
+                        Home
+                    </a>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg
+                            class="mx-1 h-3 w-3 text-base-100/50 rtl:rotate-180"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 6 10"
+                        >
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="ms-1 text-sm font-medium text-base-100/70 md:ms-2">Current</span>
+                    </div>
+                </li>
+            </ol> -->
+        </template>
+        <!-- Title -->
+        <template #pageTitle>
+            <div class="text-nowrap">Selamat Datang di,</div>
+            <div class="mt-2 w-fit rounded bg-gradient-to-r from-primary-500 to-secondary-500 p-2 text-nowrap">KONI Ketapang</div>
+        </template>
+        <!-- Description -->
+        <template #pageDescription> </template>
+        <!-- Content -->
+        <template #content>
+            <div class="mx-auto mb-10 -mt-20 rounded-lg border-border bg-gradient-to-br from-primary-400 to-primary-600 p-6 shadow-sm">
+                <div class="flex gap-5 pb-4">
+                    <Megaphone class="mt-1 h-6 w-6 text-base-50" />
+
+                    <span class="text-2xl font-semibold tracking-tight text-base-50">Pengumuman</span>
+                </div>
+                <p class="mb-3 font-normal text-base-50/90">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                <Link href="#" class="inline-flex items-center font-medium text-base-50 underline hover:opacity-80">
+                    Lihat disini selengkapnya
+                    <SquareArrowOutUpRight class="ms-1 -mt-2 h-4 w-4" />
+                </Link>
             </div>
-        </div>
-        <div class="h-screen"></div>
+            <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="" />
+                </div>
+            </div>
+        </template>
     </AppLayout>
 </template>
